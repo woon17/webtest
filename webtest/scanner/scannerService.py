2fb0c230a675c705fc302842916acf4ca0aa16a4
WEN SHUFA <shufa17@gmail.com>
2021-09-09 18:10:23 +0800

    add featuure to check with database first

--- webtest/scanner/scannerService.py
+++ webtest/scanner/scannerService.py
@@ -6,6 +6,7 @@ import json
 import ast
 from .models import Report
 import sys
+from .utils import sha256sum
 sys.path.append(".")
 from webtest.settings import BASE_DIR as baseDir
 load_dotenv()
@@ -17,7 +18,6 @@ def getId(fileDir):
     f = open(fileDir, 'rb')
     file = {"file":(fileDir, f)}
     resp = requests.post('https://www.virustotal.com/api/v3/files', files=file, headers={'X-Apikey': api_key})
-    # os.remove(fileDir)
     f.close()
     os.remove(fileDir)
 
@@ -29,24 +29,31 @@ def getDataService(fileName):
     print(f'file: {fileName}')
     fileDir = os.path.join(baseDir, "temp", fileName)
     print(f'fileDir: {fileDir}')
+    hashPre = sha256sum(fileDir)
+    print(f"hashPre: {hashPre}")
+    reportObj=Report.objects.filter(hash=hashPre)
+    if reportObj.exists():
+        print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        stats = ast.literal_eval(reportObj[0].stats)
+        print(f'stats: {stats}; {type(stats)}')
+        redirectUrl = "https://www.virustotal.com/gui/file/" +reportObj[0].hash
+        os.remove(fileDir)
+        return {'stats': stats, 
+            'redirectUrl': redirectUrl, 'status': 'completed'}
+
     id=getId(fileDir)
 
     resp2 = requests.get('https://www.virustotal.com/api/v3/analyses/{}'.format(id), headers={'x-apikey': api_key,
                                             'Accept': 'application/json'})
     print("********************************************************************************************")
     print(resp2.json())
-    # reportObj=Report.objects.filter(reportId=id)
-    # if reportObj.exists():
-    #     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    #     stats = ast.literal_eval(reportObj[0].stats)
-    #     print(f'stats: {stats}; {type(stats)}')
-    #     redirectUrl = "https://www.virustotal.com/gui/file/" +reportObj[0].hash
-    #     return {'stats': stats, 
-    #         'redirectUrl': redirectUrl}
+
 
     print(f'resp2: {resp2}')
     print(f'resp2: {resp2.json().keys()}')
     stats=resp2.json()['data']['attributes']['stats']
+    status=resp2.json()['data']['attributes']['status']
+
     hash= resp2.json()['data']['links']['item'].split('api/v3/files/')[1]
     redirectUrl = "https://www.virustotal.com/gui/file/" + hash
     '''
@@ -67,17 +74,18 @@ def getDataService(fileName):
     print(int(malicious))
     print(f'id: {id}')
     print(f'hash: {hash}')
-    # Report.objects.create(
-    #     reportId=id,
-    #     stats=stats,
-    #     hash=hash,
-    #     file=fileName
-    # )
+    print(f'status: {status}')
+    print(status == "completed")
+    if status == "completed":
+        Report.objects.create(
+            reportId=id,
+            stats=stats,
+            hash=hash,
+            file=fileName
+        )
 
     print(f'stats: {stats}; {type(stats)}')
+    print(f'status: {status}')
 
     return {'stats': stats, 
-            'redirectUrl': redirectUrl}
-
-# stats: {'harmless': 0, 'type-unsupported': 16, 'suspicious': 0, 'confirmed-timeout': 0, 'timeout': 1, 'failure': 0, 'malicious': 0, 'undetected': 56}; <class 'str'>
-# stats: {'harmless': 0, 'type-unsupported': 0, 'suspicious': 0, 'confirmed-timeout': 0, 'timeout': 0, 'failure': 0, 'malicious': 0, 'undetected': 0}; <class 'dict'>
+            'redirectUrl': redirectUrl, 'status': status}
