0839e70e147b9fd15eabbe6b5e89044774b41171
WEN SHUFA <shufa17@gmail.com>
2021-09-09 19:12:25 +0800

    change debug to false

--- webtest/scanner/views.py
+++ webtest/scanner/views.py
@@ -10,14 +10,8 @@ def index(request):
 def simple_upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
-        print(f'\nmyfile: {myfile}')
-
-        print(f'\ntype: {type(myfile)}')
-
-
         fs = FileSystemStorage("temp/")
         filename = fs.save(myfile.name, myfile)
-        print(f'filename: {filename}; type: {type(filename)}')
         # uploaded_file_url = fs.url(filename)
 
         context = {}
--- webtest/webtest/settings.py
+++ webtest/webtest/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-so*2&uepqf83bo5248qt_mivoz)x#0ew04av%a_pcj---w9z4g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
